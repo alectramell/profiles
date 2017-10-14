621206ef2ff3b4457f1f61fe902022500783bf86
alectramell <alectramell@gmail.com>
2017-10-14 16:32:44 -0700

    profiles

--- profile.sh
+++ profile.sh
@@ -6,61 +6,62 @@ source <(curl -s https://raw.githubusercontent.com/alectramell/beacons/master/co
 
 clear
 
-PROFILE=($(curl -s https://raw.githubusercontent.com/alectramell/profiles/master/$1.txt))
-
-clear
-
-NAME=$(showhex "${PROFILE[0]}")
-YOB=$(showhex "${PROFILE[1]}")
-GEN=$(showhex "${PROFILE[2]}")
-LVL=$(showhex "${PROFILE[3]}")
-STATUS=$(showhex "${PROFILE[4]}")
-PHONE=$(showhex "${PROFILE[5]}")
-
-clear
-
-YEAR=$(date +%Y)
-
-clear
-
-AGE=$(echo $(($YEAR-$YOB)))
-
-clear
-
-echo -e "\n"
-echo -n "${green}NAME:${reset} "
-echo "${white}$NAME${reset}"
-echo -n "${green}YOB:${reset} "
-echo "${white}$YOB${reset}"
-echo -n "${green}AGE:${reset} "
-echo "${white}$AGE${reset}"
-echo -n "${green}GEN:${reset} "
-echo "${white}$GEN${reset}"
-echo -n "${green}CLASS:${reset} "
-if [ $LVL = "SILVER" ]
+if [ -e https://raw.githubusercontent.com/alectramell/profiles/master/$1.txt ]
 then
-	echo "${sky}$LVL${reset}"
+	PROFILE=($(curl -s https://raw.githubusercontent.com/alectramell/profiles/master/$1.txt))
+	clear
+	NAME=$(showhex "${PROFILE[0]}")
+	YOB=$(showhex "${PROFILE[1]}")
+	GEN=$(showhex "${PROFILE[2]}")
+	LVL=$(showhex "${PROFILE[3]}")
+	STATUS=$(showhex "${PROFILE[4]}")
+	PHONE=$(showhex "${PROFILE[5]}")
+	clear
+	YEAR=$(date +%Y)
+	clear
+	AGE=$(echo $(($YEAR-$YOB)))
+	clear
+	echo -e "\n"
+	echo -n "${green}NAME:${reset} "
+	echo "${white}$NAME${reset}"
+	echo -n "${green}YOB:${reset} "
+	echo "${white}$YOB${reset}"
+	echo -n "${green}AGE:${reset} "
+	echo "${white}$AGE${reset}"
+	echo -n "${green}GEN:${reset} "
+	echo "${white}$GEN${reset}"
+	echo -n "${green}CLASS:${reset} "
+	if [ $LVL = "SILVER" ]
+	then
+		echo "${sky}$LVL${reset}"
 
-elif [ $LVL = "GOLD" ]
-then
-	echo "${gold}$LVL${reset}"
+	elif [ $LVL = "GOLD" ]
+	then
+		echo "${gold}$LVL${reset}"
+	else
+		echo "${white}$LVL${reset}"
+	fi
+	echo -n "${green}STATUS:${reset} "
+	if [ $STATUS = "ACTIVE" ]
+	then
+		echo "${yellow}$STATUS${reset}"
+
+	elif [ $STATUS = "INACTIVE" ]
+	then
+		echo "${red}$STATUS${reset}"
+	else
+		echo ""${purple}$STATUS${reset}""
+	fi
+	echo -n "${green}PHONE:${reset} "
+	echo "${white}$PHONE${reset}"
+	echo -e "\n" | pg
+	clear
 else
-	echo "${white}$LVL${reset}"
+	clear
+	echo -n "${red}ERROR${reset} ${white}That Profile Does Not Exist!${reset}"
+	read -s -n1
+	clear
 fi
-echo -n "${green}STATUS:${reset} "
-if [ $STATUS = "ACTIVE" ]
-then
-	echo "${yellow}$STATUS${reset}"
-
-elif [ $STATUS = "INACTIVE" ]
-then
-	echo "${red}$STATUS${reset}"
-else
-	echo ""${purple}$STATUS${reset}""
-fi
-echo -n "${green}PHONE:${reset} "
-echo "${white}$PHONE${reset}"
-echo -e "\n" | pg
 
 clear
 
